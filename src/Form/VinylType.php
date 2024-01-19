1bce64c6b93ddba5ce74d5529f75515a98f1be85
julien <julien.touchard@free.fr>
2024-01-19 12:19:46 +0100

    first commit

---
+++ src/Form/VinylType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Vinyl;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
+
+class VinylType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre')
+            ->add('artiste')
+            ->add('annee')
+            ->add('cover', FileType::class, [
+                'mapped' => false,
+                'label' => 'Fichier image',
+                'constraints' => [
+                    new File([
+                        'maxSize' => '4096k',
+                        'mimeTypes' => [
+                            'image/jpg',
+                            'image/jpeg',
+                            'image/png',
+                            'image/webp',
+                            'image/gif'
+                        ],
+                        'mimeTypesMessage' => "ceci n'est pas une image valide"
+                    ])
+                ]
+            ])
+            ->add('audio')
+            ->add('album');
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Vinyl::class,
+        ]);
+    }
+}
